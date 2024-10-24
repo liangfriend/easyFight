59a27d23893960853f04131a948f7bd82a5253f4
Liam <2217356256@qq.com>
2024-10-24 19:51:05 +0800

    同步

--- assets/scripts/LeftPlayer.ts
+++ assets/scripts/LeftPlayer.ts
@@ -30,24 +30,31 @@ export class LeftPlayer extends Player {
         isMoveRight: false,
     };
 
+    private bulletAttributes = {
+        //子弹轮盘运动方向
+        bulletPrepareDirection: -1, //1 -1
+        currentAngle: 0,
+    };
+
     protected onLoad(): void {
         this.body = this.node.getComponent(RigidBody2D);
         this.BulletPoint = this.node.getChildByName('BulletPoint');
+        this.direction = 'RIGHT';
         this.move();
         this.collider();
-    }
-    start() {
-        this.direction = 'RIGHT';
     }
     update(deltaTime: number) {
         const x = this.node.getPosition().x;
         const y = this.node.getPosition().y;
+        //向左移动
         if (this.moveAction.isMoveLeft) {
             this.node.setPosition(x - this.speed, y);
         }
+        //向右移动
         if (this.moveAction.isMoveRight) {
             this.node.setPosition(x + this.speed, y);
         }
+        //跳跃
         if (this.moveAction.isMoveUp) {
             this.moveAction.isMoveUp = false;
             const velocity = this.body.linearVelocity;
@@ -55,24 +62,55 @@ export class LeftPlayer extends Player {
             this.body.linearVelocity = velocity;
         }
         if (this.moveAction.isMoveDown) {
+            //
         }
 
         if (this.BulletReady) {
             tween(this.BulletPoint).show();
-            let angle = (this.BulletPoint.rotation.z / Math.PI) * 360;
-            console.log('执行', this.BulletReady, angle);
+            // (this.BulletPoint.rotation.z / Math.PI) * 360;
+            console.log(
+                '执行',
+                this.BulletReady,
+                this.bulletAttributes.currentAngle,
+                this.direction,
+                this.bulletAttributes.bulletPrepareDirection
+            );
 
             if (this.direction === 'LEFT') {
-                if (angle < 90) {
-                    angle += 0.1;
+                if (
+                    this.bulletAttributes.currentAngle === 0 ||
+                    this.bulletAttributes.currentAngle === -90
+                ) {
+                    this.bulletAttributes.bulletPrepareDirection *= -1;
+                }
+                if (
+                    this.bulletAttributes.currentAngle <= 0 ||
+                    this.bulletAttributes.currentAngle >= -90
+                ) {
+                    this.bulletAttributes.currentAngle +=
+                        1 * this.bulletAttributes.bulletPrepareDirection;
                 }
             } else if (this.direction === 'RIGHT') {
-                if (angle < 90) {
-                    angle += 0.1;
+                if (
+                    this.bulletAttributes.currentAngle === 0 ||
+                    this.bulletAttributes.currentAngle === 90
+                ) {
+                    this.bulletAttributes.bulletPrepareDirection *= -1;
+                }
+                if (
+                    this.bulletAttributes.currentAngle <= 90 ||
+                    this.bulletAttributes.currentAngle >= 0
+                ) {
+                    this.bulletAttributes.currentAngle +=
+                        1 * this.bulletAttributes.bulletPrepareDirection;
                 }
             }
             this.BulletPoint.setRotation(
-                new Quat(0, 0, (Math.PI * angle) / 360)
+                new Quat(
+                    0,
+                    0,
+                    (Math.PI * this.bulletAttributes.currentAngle) / 360
+                )
             );
         }
     }
@@ -91,12 +129,14 @@ export class LeftPlayer extends Player {
             }
             if (event.keyCode === keys['a']) {
                 this.moveAction.isMoveLeft = true;
+                this.setDirection('LEFT');
             }
             if (event.keyCode === keys['s']) {
                 this.moveAction.isMoveDown = true;
             }
             if (event.keyCode === keys['d']) {
                 this.moveAction.isMoveRight = true;
+                this.setDirection('RIGHT');
             }
             if (event.keyCode === keys['h']) {
                 this.BulletReady = true;
@@ -109,20 +149,27 @@ export class LeftPlayer extends Player {
             }
             if (event.keyCode === keys['a']) {
                 this.moveAction.isMoveLeft = false;
-                this.direction = 'LEFT';
             }
             if (event.keyCode === keys['s']) {
                 this.moveAction.isMoveDown = false;
             }
             if (event.keyCode === keys['d']) {
                 this.moveAction.isMoveRight = false;
-                this.direction = 'RIGHT';
             }
             if (event.keyCode === keys['h']) {
                 this.BulletReady = false;
             }
         });
         input.on(Input.EventType.KEY_PRESSING, (event) => {});
+    }
+    setDirection(value) {
+        if (value !== this.direction) {
+            this.direction = value;
+            //翻转
+            const s = this.node.getScale();
+            this.node.setScale(s.x * -1, s.y);
+        } else {
+        }
     }
     onCollisionEnter(
         selfCollider: Collider2D,
